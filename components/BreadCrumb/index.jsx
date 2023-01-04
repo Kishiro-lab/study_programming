9fc641c856cc96729a704c9e8c4d82e60ac21b02
kishiro <the.yuhei@gmail.com>
2023-01-04 18:10:26 +0900

    🎨 コンポーネントの作成

---
+++ components/BreadCrumb/index.jsx
@@ -0,0 +1,12 @@
+import classes from "components/BreadCrumb/Bread.module.css"
+
+export const Bread = () => {
+  return(
+    <div className={classes.breadcrumb}>
+      <p>ここはパンくず</p>
+
+    </div>
+
+  )
+}
+
---
+++ components/Content/index.jsx
@@ -0,0 +1,23 @@
+import classes from 'components/Content/Content.module.css'
+
+export const Content = () => {
+  return(
+    <div className={classes.content}>
+
+    <main>
+      ここはコンテンツ 
+
+        
+    </main>
+
+    <div className={classes.sidebar}>
+      ここはサイドバー
+
+    </div>
+
+
+
+
+    </div>
+  )
+}
